0a0764571afd30860d7e6d45b8b7ff198e6ba928
SolaraTerminal <wedew2749@gmail.com>
2024-12-03 22:24:52 +0800

    Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Welcome to Solara Blockchain Explorer</h1>
+      <p>Explore transactions, blocks, and contracts in real-time.</p>
+    </header>
+  );
+};
+
+export default Header;
