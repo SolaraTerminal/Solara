0f7b30f281c4928fe5325f6de413e0bdc1da74a8
SolaraTerminal <wedew2749@gmail.com>
2024-12-03 22:28:09 +0800

    App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import BlockchainView from './components/BlockchainView';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <BlockchainView />
+    </div>
+  );
+};
+
+export default App;
